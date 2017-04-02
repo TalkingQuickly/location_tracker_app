1ffc245fe570002ee63f0df640f3d4bc06d9ca5e
Ben Dixon <ben@hillsbede.co.uk>
2017-04-02 21:53:23 +0100

    adds: redux chrome dev tools support

--- App/Web/Redux/CreateStore.js
+++ App/Web/Redux/CreateStore.js
@@ -31,6 +31,8 @@ export default (rootReducer, rootSaga, startupCallback) => {
     enhancers.push(autoRehydrate())
   }
 
+  enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
   const store = createStore(rootReducer, compose(...enhancers))
 
   // configure persistStore and check reducer version number
